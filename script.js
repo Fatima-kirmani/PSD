9b4c956bdabeecb865b0e062244cdcf3cf369acb
Fatima-kirmani <fatimakirmani56@gmail.com>
2020-07-24 11:01:16 +0500

    CSS assignment

---
+++ script.js
@@ -0,0 +1,12 @@
+// $(document).ready(function(){
+//     $('.header-slider').slick({
+//         infinite: false,
+//         slidesToShow: 6,
+//         slidesToScroll: 3,
+//         arrows: true,
+//         prevArrow: true,
+//         nextArrow: true,
+//         cssEase:'ease',
+//         easing:'linear',
+//     });
+//   });
